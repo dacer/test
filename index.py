dd30e2f9fae1572a58550cf809557df6122ef1ab
Dacer <dacerfeedback@gmail.com>
2023-03-05 15:59:11 +0900

    test

--- index.py
+++ index.py
@@ -1,5 +1,15 @@
 from real_url.douyu import DouYu
+import json
 
-s = DouYu(74751)
-s.did = s.get_did()
-print(s.get_real_url())
+def write_playlist():
+    s = DouYu(74751)
+    s.did = s.get_did()
+    urls = json.loads(s.get_real_url())
+    with open("playlist.m3u8", "w") as f:
+        f.write("#EXTM3U\n")
+        for i, url in enumerate(urls.values()):
+            f.write(f"#EXTINF:-1, Source({i})\n")
+            f.write(f"{url}\n")
+
+if __name__ == "__main__":
+    write_playlist()
